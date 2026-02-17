6ed3f5e7a40375998e0843e4a3fc830081691c34
zepp <jabbarirhamabdul0@gmail.com>
2026-02-17 22:02:07 +0700

    Add comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,11 +4,11 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
     static class bidangDuaDimensi{
-        private String nama;
+        private String nama; //atribut class bidangDuaDimensi
 
         public bidangDuaDimensi(String nama){
             this.nama = nama;
-        }
+        } //constructor
 
         public void info(){
             System.out.println(nama);
@@ -18,28 +18,34 @@ public class App {
 
         }
 
+        //child class dari class bidangDuaDimensi
         class bujurSangkar extends bidangDuaDimensi {
-            private float sisi;
+            private float sisi; //atribut class bujurSangkar
 
+            //constructor
             public bujurSangkar(String nama, float sisi) {
                 super(nama);
                 this.sisi = sisi;
             }
 
+            //overriding method yang sama dari parent class
             @Override
             public void luas() {
                 System.out.println(sisi * sisi);
             }
         }
 
+        //child class dari class bidangDuaDimensi
         class lingkaran extends bidangDuaDimensi{
-            private float jariJari;
+            private float jariJari; //atribut class lingkaran
 
+            //constructor
             public lingkaran(String nama, float jariJari) {
                 super(nama);
                 this.jariJari = jariJari;
             }
 
+            //overriding method yang sama dari parent class
             @Override
             public void luas() {
                 System.out.println(Math.PI * jariJari * jariJari);
